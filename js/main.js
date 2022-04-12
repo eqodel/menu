d4b35f7a1bf3afc4550a30682efe2555aea7e3b4
Ilya <86313716+eqodel@users.noreply.github.com>
2022-04-12 14:43:08 +0300

    create

---
+++ js/main.js
@@ -0,0 +1,30 @@
+for (let li of list.querySelectorAll('li')) {
+    let span = document.createElement('span'); /* создание span */
+    span.classList.add('show'); /* добавление класса для span */
+    li.prepend(span); /* вставить span внутри li */
+    span.append(span.nextSibling) /* присоединить к span следующий узел */
+
+    list.onclick = function(event) {
+        if (event.target.tagName != 'SPAN') return;
+    
+        let childrenList = event.target.parentNode.querySelector('ul');
+        if (!childrenList) return;
+        childrenList.hidden = !childrenList.hidden;
+    
+        if (childrenList.hidden) {
+            event.target.classList.add('hide');
+            event.target.classList.remove('show');
+        }
+    
+        else {
+            event.target.classList.add('show');
+            event.target.classList.remove('hide');
+        }
+    }    
+}
+
+var menuButton = $('.header__menu')
+menuButton.on('click', () => {
+    $('.menu').toggleClass('menu--visible')
+    $('.menu__overlay').toggleClass('menu__overlay--visible')
+})
